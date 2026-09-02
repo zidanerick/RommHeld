34885608b7f1616924416fee4502306694832ffd
zidanerick <ricky@patton.id.au>
2026-09-02 19:19:18 +1000

    test: cover installable 3DS target

--- tests/test_three_ds_targets.py
+++ tests/test_three_ds_targets.py
@@ -11,11 +11,17 @@ def test_gb_exposes_retroarch_and_vc_targets():
     assert keys == ["retroarch", "vc_cia"]
 
 
+def test_3ds_exposes_existing_cia_target():
+    assert compatible_platform("3ds")
+    assert [target.key for target in available_targets("3ds")] == ["native_3ds_cia"]
+
+
 def test_non_supported_platform_is_not_claimed_compatible():
-    assert not compatible_platform("3ds")
+    assert not compatible_platform("ps2")
 
 
 def test_default_destinations_are_stable_and_explicit():
     assert default_destination("retroarch", "gba", "Metroid Fusion.gba") == "/RetroArch/roms/gba/Metroid Fusion.gba"
-    assert default_destination("native_gba", "gba", "Metroid Fusion.gba") == "/roms/gba/Metroid Fusion.gba"
+    assert default_destination("native_gba", "gba", "Metroid Fusion.gba") == "/cias/Metroid Fusion.cia"
     assert default_destination("vc_cia", "gba", "Metroid Fusion.gba") == "/cias/Metroid Fusion.cia"
+    assert default_destination("native_3ds_cia", "3ds", "Metroid.3ds") == "/cias/Metroid.cia"
