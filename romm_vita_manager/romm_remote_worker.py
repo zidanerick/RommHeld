314b3faf3278073f26d2cd94a6cf32c7944b4f9a
zidanerick <ricky@patton.id.au>
2026-09-02 19:14:05 +1000

    feat: load compatible RomM library asynchronously

--- romm_vita_manager/romm_remote_worker.py
+++ romm_vita_manager/romm_remote_worker.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 
 from PySide6.QtCore import QThread, Signal
 
-from .romm_remote import RomMRemoteGame, list_3ds_games
+from .romm_remote import RomMRemoteGame, list_compatible_games, list_3ds_games
 
 
-class RomM3DSLibraryWorker(QThread):
+class RomMLibraryWorker(QThread):
     loaded = Signal(object)
     failed = Signal(str)
 
@@ -13,6 +13,16 @@ class RomM3DSLibraryWorker(QThread):
         super().__init__()
         self.instance_url = instance_url
         self.token = token
+
+    def run(self) -> None:
+        try:
+            self.loaded.emit(list_compatible_games(self.instance_url, self.token))
+        except Exception as exc:
+            self.failed.emit(str(exc))
+
+
+class RomM3DSLibraryWorker(RomMLibraryWorker):
+    """Compatibility wrapper retained for older callers and tests."""
 
     def run(self) -> None:
         try:
