cec13574cc526d1e862f4723cfeb76ced4fc48ac
zidanerick <ricky@patton.id.au>
2026-09-02 14:50:30 +1000

    Remove external Icons8 asset dependency from platform registry

--- romm_vita_manager/platform_assets.py
+++ romm_vita_manager/platform_assets.py
@@ -29,26 +29,26 @@ PLATFORM_ASSETS = {
     "vita": PlatformAssets(
         "playstation_vita", "PlayStation Vita", "device_large.svg", "device_small.svg", "logo.svg", "logo_dark.svg",
         "#111111", "#3b9cf5", True,
-        "https://icons8.com/icon/13629/playstation",
-        "Original RommHeld handheld artwork visually inspired by the user's selected Icons8 Color reference. No Icons8 asset is bundled.",
+        "https://commons.wikimedia.org/wiki/File:PlayStation_Vita_illustration.svg",
+        "Original RommHeld vector based on the physical PCH-1000 layout; visual reference cross-checked against Wikimedia Commons. The Commons reference is CC BY 3.0 and is not bundled.",
     ),
     "3ds": PlatformAssets(
-        "nintendo_3ds", "Nintendo 3DS", "device_large.svg", "device_small.svg", "logo_simpleicons.svg", "logo_simpleicons_dark.svg",
+        "nintendo_3ds", "Nintendo 3DS", "device_large.svg", "device_small.svg", "logo.svg", "logo_dark.svg",
         "#25a6c9", "#d12228", True,
-        "https://icons8.com/icon/fdVLfvnTjw0H/3ds-console",
-        "Original RommHeld handheld artwork visually inspired by the user's selected Icons8 Color reference. Platform logo remains separately sourced.",
+        "https://commons.wikimedia.org/wiki/File:Nintendo-3DS-AquaOpen.png",
+        "Original RommHeld vector based on the launch-era Aqua Blue hardware. The Commons reference is public domain and is not bundled.",
     ),
     "ds": PlatformAssets(
         "nintendo_ds", "Nintendo DS", "device_large.svg", "device_small.svg", "logo.svg", "logo_dark.svg",
         "#dfe3e8", "#4b83b5", True,
-        "https://icons8.com/icon/19599/nintendo-ds",
-        "Original RommHeld handheld artwork visually inspired by the user's selected Icons8 Color reference. Platform logo remains separately sourced.",
+        "https://commons.wikimedia.org/wiki/File:Nintendo_DS.svg",
+        "Original RommHeld vector based on the physical Nintendo DS layout. The Commons reference is CC0 and is not bundled.",
     ),
     "psp": PlatformAssets(
         "playstation_portable", "PlayStation Portable", "device_large.svg", "device_small.svg", "logo.svg", "logo_dark.svg",
         "#111111", "#777b84", True,
-        "https://icons8.com/icon/xv4xoRkYlmXq/playstation-portable",
-        "Original RommHeld handheld artwork visually inspired by the user's selected Icons8 Color reference. PSP remains a coming-soon target.",
+        "https://commons.wikimedia.org/wiki/File:PlayStation_Portable.svg",
+        "Original RommHeld vector based on the PSP hardware layout. PSP remains a coming-soon target.",
     ),
 }
 
