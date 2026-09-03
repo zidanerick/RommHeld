c28a9cd6dc39f10100add5d32d8eabe1d01e720f
zidanerick <ricky@patton.id.au>
2026-09-03 17:01:26 +1000

    Test bundled GBA boot-logo fallback

--- tests/test_gba_vc.py
+++ tests/test_gba_vc.py
@@ -1,3 +1,4 @@
+from romm_vita_manager.gba_boot_logo import LOGO_REGION_SIZE, bundled_boot_logo
 from romm_vita_manager.gba_vc import native_title_id_for_romm_id
 
 
@@ -11,3 +12,8 @@ def test_native_title_id_is_valid_gba_vc_shape():
 def test_native_title_id_is_stable():
     assert native_title_id_for_romm_id(42) == native_title_id_for_romm_id(42)
     assert native_title_id_for_romm_id(42) != native_title_id_for_romm_id(43)
+
+
+def test_bundled_boot_logo_has_native_logo_region_size():
+    logo = bundled_boot_logo()
+    assert len(logo) == LOGO_REGION_SIZE == 0x2000
