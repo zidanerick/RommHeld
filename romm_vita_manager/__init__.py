95a528509cd48ce547b38755beb780e2d9cbb849
zidanerick <ricky@patton.id.au>
2026-09-04 19:32:08 +1000

    Install SNES VC family before shared validators

--- romm_vita_manager/__init__.py
+++ romm_vita_manager/__init__.py
@@ -5,6 +5,7 @@
 # symbols so every deployment path sees the same validated implementation.
 from .nes_vc import install as _install_nes_vc
 from .gamegear_vc import install as _install_gamegear_vc
+from .snes_vc import install as _install_snes_vc
 from .classic_vc_title_fix import install as _install_classic_vc_title_fix
 from .classic_vc_hardware_fix import install as _install_classic_vc_hardware_fix
 from .classic_vc_presentation_fix import install as _install_classic_vc_presentation_fix
@@ -12,6 +13,7 @@ from .gba_vc_presentation_compat import install as _install_gba_vc_presentation_
 
 _install_nes_vc()
 _install_gamegear_vc()
+_install_snes_vc()
 _install_classic_vc_title_fix()
 _install_classic_vc_hardware_fix()
 _install_classic_vc_presentation_fix()
@@ -19,6 +21,7 @@ _install_gba_vc_presentation_compat()
 
 del _install_nes_vc
 del _install_gamegear_vc
+del _install_snes_vc
 del _install_classic_vc_title_fix
 del _install_classic_vc_hardware_fix
 del _install_classic_vc_presentation_fix
