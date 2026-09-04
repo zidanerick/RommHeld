fd55432e4d315de56efcb157188a28f2651824d5
zidanerick <ricky@patton.id.au>
2026-09-04 15:27:47 +1000

    Test RomM release year metadata

--- tests/test_romm_remote_metadata.py
+++ tests/test_romm_remote_metadata.py
@@ -1,4 +1,4 @@
-from romm_vita_manager.romm_remote import _publisher_name
+from romm_vita_manager.romm_remote import _publisher_name, _release_year
 
 
 def test_publisher_name_uses_direct_romm_metadata():
@@ -25,3 +25,19 @@ def test_publisher_name_ignores_non_publisher_company_roles():
         )
         == "Publisher Co"
     )
+
+
+def test_release_year_uses_direct_year():
+    assert _release_year({"release_year": 2001}) == 2001
+
+
+def test_release_year_reads_iso_metadata_date():
+    assert _release_year({"metadata": {"first_release_date": "1998-12-01"}}) == 1998
+
+
+def test_release_year_reads_unix_timestamp():
+    assert _release_year({"first_release_date": 978307200}) == 2001
+
+
+def test_release_year_rejects_implausible_year():
+    assert _release_year({"release_year": 42}) is None
